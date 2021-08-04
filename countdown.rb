dc27b563ed06ec845489d5dcd4a7c70c30c795d5
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-04 14:59:31 -0400

    Completed Assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = num
+  while counter >= 1 do
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  sleep(num)
 end
